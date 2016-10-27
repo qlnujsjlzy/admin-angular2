ce33f794f14d60bcdb7aa22863960d400a70a8ce
licy13 <licy13@lenovo.com>
2016-10-27 14:29:27 +0800

    amend
    Signed-off-by: licy13 <licy13@lenovo.com>

---
+++ src/app/admin/dashboard/navbar/topnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'admin-topnavbar',
+    templateUrl: './topnavbar.component.html',
+})
+export class TopnavbarComponent {
+    a7Img = require('./_images/a7.jpg');
+    a4Img = require('./_images/a4.jpg');
+    profileImg = require('./_images/profile.jpg');
+}
